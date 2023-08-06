07fe3911b95e94aa178b3c4c17d6a4ffa0c2bf93
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-08-06 22:30:38 +0200

    fix build

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
-import theme from './Theme/theme';
+import theme from './Theme/Theme.tsx';
 import { App } from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
