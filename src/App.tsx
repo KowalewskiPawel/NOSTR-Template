0b00e18e4359e9c8ea19b3a5fe50050a7bac8cc9
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-08-06 20:45:14 +0200

    add custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,11 @@
-import { SimplePool, Event } from "nostr-tools";
-import { useEffect, useRef, useState } from "react";
-import { useDebounce } from "use-debounce";
 import "./App.css";
 import { CreateNote } from "./Components/CreateNote";
 import { HashtagsFilter } from "./Components/HashtagsFilter";
 import { NotesList } from "./Components/NotesList";
-import { insertEventIntoDescendingList } from "./utils";
-import { RELAYS } from "./consts";
-import type { Metadata } from "./types";
+import { useGetPool } from "./hooks/useGetPool";
 
-function App() {
-  const [pool, setPool] = useState<SimplePool | null>(null);
-
-  const [eventsImmediate, setEvents] = useState<Event[]>([]);
-
-  const [events] = useDebounce(eventsImmediate, 1500);
-
-  const [metadata, setMetadata] = useState<Record<string, Metadata>>({});
-
-  const metadataFetched = useRef<Record<string, boolean>>({});
-
-  const [hashtags, setHashtags] = useState<string[]>([]);
-
-  // setup a relays pool
-
-  useEffect(() => {
-    const _pool = new SimplePool();
-    setPool(_pool);
-
-    return () => {
-      _pool.close(RELAYS);
-    };
-  }, []);
-
-  // subscribe to some events
-  useEffect(() => {
-    if (!pool) return;
-
-    setEvents([]);
-    const sub = pool.sub(RELAYS, [
-      {
-        kinds: [1],
-        limit: 100,
-        "#t": hashtags,
-      },
-    ]);
-
-    sub.on("event", (event: Event) => {
-      setEvents((events) => insertEventIntoDescendingList(events, event));
-    });
-
-    return () => {
-      sub.unsub();
-    };
-  }, [hashtags, pool]);
-
-  useEffect(() => {
-    if (!pool) return;
-
-    const pubkeysToFetch = events
-      .filter((event) => metadataFetched.current[event.pubkey] !== true)
-      .map((event) => event.pubkey);
-
-    pubkeysToFetch.forEach(
-      (pubkey) => (metadataFetched.current[pubkey] = true)
-    );
-
-    const sub = pool.sub(RELAYS, [
-      {
-        kinds: [0],
-        authors: pubkeysToFetch,
-      },
-    ]);
-
-    sub.on("event", (event: Event) => {
-      const metadata = JSON.parse(event.content) as Metadata;
-
-      setMetadata((cur) => ({
-        ...cur,
-        [event.pubkey]: metadata,
-      }));
-    });
-
-    sub.on("eose", () => {
-      sub.unsub();
-    });
-
-    return () => {};
-  }, [events, pool]);
+export const App = () => {
+  const { pool, hashtags, setHashtags, metadata, events } = useGetPool();
 
   if (!pool) return null;
 
@@ -101,6 +19,4 @@ function App() {
       </div>
     </div>
   );
-}
-
-export default App;
+};
